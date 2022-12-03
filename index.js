0881c4b12ccadd6085e1a9d10036547d9ad1cc83
Trent Brew <tbrew1023@gmail.com>
2022-12-03 01:16:25 -0600

    vercel config

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const fs = require("fs");
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
